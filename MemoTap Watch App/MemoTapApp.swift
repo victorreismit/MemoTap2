d04c6a89f39cdeeb7373b04cd4a957b24420e82c
Victor Reis <marq25@manukaumail.com>
2024-08-07 16:10:36 +1200

    Initial Commit

---
+++ MemoTap Watch App/MemoTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoTapApp.swift
+//  MemoTap Watch App
+//
+//  Created by Victor Reis on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoTap_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
